8c3582270126263588040331e33d220d510039b6
Andrew Chan <andrew.chan9494@yahoo.com>
2017-02-15 12:04:06 +0000

    update

--- samplecode/example.py
+++ samplecode/example.py
@@ -3,15 +3,13 @@ import pprint
 import numpy as np
 
 
+# open any pkl file you like
 
 # make sure to change the path to your file
-path_to_pkl1_actors = '/Users/Andrew/Github-repo/movie_credits/cleandata/actor_dict_lite.pkl'
-path_to_pkl2_movies = '/Users/Andrew/Github-repo/movie_credits/cleandata/movie_dict_lite.pkl'
+PATH_TO_FILE = '/Users/Andrew/Github-repo/movie_credits/cleandata/actor_dict_lite.pkl'
 
-with open(path_to_pkl1_actors, mode='rb') as actors, open(path_to_pkl2_movies, mode='rb') as movies:
-    actor = pickle.load(actors)
-    movie = pickle.load(movies)
+with open(PATH_TO_FILE, mode='rb') as file:
+    reader = pickle.load(file)
 
 # if you want to see it, uncomment
-#pprint.pprint(actor)
-# pprint.pprint(movie)
+#pprint.pprint(reader)
