a714e1c62a581357f1d2fa6ed18ab90ac8d17213
Andrew Chan <andrew.chan9494@yahoo.com>
2017-02-19 00:07:20 +0000

    update: simplified the program for users, and made file creation cross-platform.

--- cleandata/datacleaning.py
+++ cleandata/datacleaning.py
@@ -5,32 +5,24 @@ TOTALMOVIES = 1042622
 #Actor = namedtuple('Actor', ['name', 'extra'])
 
 # please change the the path
-INPUT = "/Users/Andrew/Github-repo/movie_credits/2017-02-09_223251_ImdbParserOutput/actors.list.tsv"
+INPUT = "2017-02-09_223251_ImdbParserOutput/actors.list.tsv"
 OUTPUT = 'map_lite.pkl'
 
 # generate desired files
 GENERATE = [
-    #'actor movie'
-    #'unique actor movie'
+    #'actor movie',
+    #'unique actor movie',
     'filtered csv'
 ]
 
 make = generate.Generate(INPUT, stop=100000)
 
-options = {'actor movie': make.actor_movie(),
-           'unique actor movie': make.unique_actor_movie(),
-           'filtered csv': make.filtered_csv()
+options = {'actor movie': make.actor_movie,
+           'unique actor movie': make.unique_actor_movie,
+           'filtered csv': make.filtered_csv
            }
-
 for option in GENERATE:
-    options.get(option)
-
-"""
-if not filehandler.exist('movie2actor.pkl'):
-    make.actor_movie()
-#make.unique_actor_movie()
-make.filtered_csv()
-"""
+    options.get(option)()
 
 
 def main():
@@ -41,7 +33,7 @@ def main():
     with open('movie2actor.pkl', mode='rb') as file:
         movie2actors = pickle.load(file)
 
-    print(movie2actors)
+    #print(movie2actors)
 
 
 def detailed_mapping():
--- cleandata/mc/utils/filehandler.py
+++ cleandata/mc/utils/filehandler.py
@@ -1,36 +1,11 @@
 import os
-import subprocess
-from sys import platform as _platform, exit
 
 def create(file):
-# check file existence, create file if required
-    if bash():
-        if not os.path.isfile(file):
-            command = "touch %s"% file
-            subprocess.run([command], shell=True)
-            print("{} does not exist... creating one for you.".format(file))
-    elif not bash():
-        if not os.path.isfile(file):
-            command = "type nul >> %s"% file
-            subprocess.run([command], shell=True)
-            print("{} does not exist... creating one for you.".format(file))
-    else:
-        exit("Error: Uncertain of the operating system you are using")
+# create file if required
+   with open(file, mode='a'):
+       os.utime(file, None) # set modified time
 
 def exist(file):
     return os.path.isfile(file)
-
-def bash():
-    """
-    Check bash compliant
-    :return: Boolean
-    """
-    if _platform == "linux" or _platform == "linux2":
-        return True
-    elif _platform == "darwin":
-        return True
-    elif _platform == "win32":
-        return False
-
 
 
