8103c32ed90ff4b94a89a218bf28927e77072210
Andrew Chan <andrew.chan9494@yahoo.com>
2017-02-24 08:01:26 +0000

    Change: mapping from actor 2 movies and movies 2 actors read from the csv.

--- moviecredits/connections/connections.py
+++ moviecredits/connections/connections.py
@@ -1,6 +1,9 @@
 from moviecredits.datacleaning import INPUT as FILE_DIR
 from moviecredits.utils import generate
-from typing import Set
+from typing import Set, Dict
+from collections import namedtuple, Counter, defaultdict
+import itertools
+import pprint
 
 def convert_to_actor_name(ids: Set):
     return [id2actors.get(id) for id in list(ids)]
@@ -8,6 +11,57 @@ def convert_to_actor_name(ids: Set):
 def convert_to_movie_name(id):
     """ids: Integer"""
     return id2movies.get(id)
+
+# TODO End goal to be able to see the connections like this. In order to create a adjacency matrix
+# TODO Currently at Generating the actor2actor, next use Counter.
+connect = namedtuple('Connection', ['ActorA', 'ActorB', 'Weight'])
+
+class Link:
+
+    def __init__(self, actor2movies: Dict, movie2actors: Dict):
+        self.actor2movies = actor2movies
+        self.movie2actors = movie2actors
+        self._actor2actors = defaultdict(list)
+
+    def actor2actors(self):
+
+        # go through the movies
+        for actor, movies in self.actor2movies.items():
+            for movie in movies:
+                # return cast
+                cast = list(self.movie2actors.get(movie))
+                self._actor2actors[actor].append(cast)
+
+            # join list of lists
+            merged_list = list(itertools.chain.from_iterable(self._actor2actors[actor]))
+
+            # count the actors
+            times_worked_together = Counter(merged_list)
+
+
+            self._actor2actors[actor] = times_worked_together
+
+        print(self._actor2actors)
+
+    def example(self):
+
+        # go through the movies
+        for _, movies in self.actor2movies.items():
+            for movie in movies:
+
+                # convert to names
+                print("movieid: {} movie: {}, actorsid: {}, actors: {}".format(movie,
+                                                                               convert_to_movie_name(movie),
+                                                                               self.movie2actors.get(movie),
+                                                                               convert_to_actor_name(
+                                                                                   self.movie2actors.get(movie))))
+
+                # return actors
+                pairs = list(make.pair_actors(movie2actors.get(movie)))
+                if pairs:
+                    print(pairs)
+                else:
+                    print("skip, only one actor")
 
 
 make = generate.Generate(FILE_DIR, stop=100000)
@@ -17,18 +71,8 @@ top_actors = make.top_actors()
 
 movie2actors, id2actors, id2movies = make._connection("movie2actors with id2actors")
 
-# go through the movies
-for _, movies in top_actors.items():
-    for movie in movies:
+link = Link(top_actors, movie2actors)
 
-        # convert to names
-        print("movieid: {} movie: {}, actorsid: {}, actors: {}".format(movie,
-                                                                       convert_to_movie_name(movie),
-                                                                       movie2actors.get(movie),
-                                                                       convert_to_actor_name(movie2actors.get(movie))))
-        # return actors
-        pairs = list(make.pair_actors(movie2actors.get(movie)))
-        if pairs:
-            print(pairs)
-        else:
-            print("skip, only one actor")
+# pprint.pprint(link.actor2actors())
+link.actor2actors()
+
