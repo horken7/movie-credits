454fab83b1b4e914457a5ef819ce052b195968b2
Andrew Chan <andrew.chan9494@yahoo.com>
2017-02-25 20:20:03 +0000

    Show some examples.

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
 import moviecredits.connections as connections
+import numpy as np
 
 def main():
-    connections_matrix = connections.matrix()
+    actors, colleagues, connections_matrix = connections.matrix()
     print(connections_matrix)
 
-    # next... to output the past output examples to complement the matrix.
+    # next... to output the previous output examples to complement the matrix.
+
+    # the index position represents the index in the array.
+    # colleagues[0], actors[0] is:
+    print(connections_matrix[(0,0)])
+    print()
+
+    # index positions value > 0
+    colleagues_index, actor_index = np.where(connections_matrix > 0)
+
+    for index in range(len(actor_index)):
+        print("colleague {} | actor {}".format(colleagues[colleagues_index[index]], actors[actor_index[index]]))
+        print("weight %d "% connections_matrix[(colleagues_index[index], actor_index[index])])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
