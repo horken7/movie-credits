63614b46363539c38b0ec2196f5ecd73022a0f28
Andrew Chan <andrew.chan9494@yahoo.com>
2017-02-26 15:04:55 +0000

    remove tmp

--- tmp.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open ('2017-02-09_223251_ImdbParserOutput/actors.list.tsv.csv', 'r') as file:
-    reader = csv.reader(file)
-
-    for row in reader:
-        print(row)
-        break
